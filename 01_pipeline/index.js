67ed19464fa7c574d20f7f9696f2b53b0ea527bb
gersoncl <gerson.choque.loreno@gmail.com>
2025-07-17 19:00:24 -0400

    cambios for test pipeline 4

--- 01_pipeline/index.js
+++ 01_pipeline/index.js
@@ -3,11 +3,12 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('¡Hola Mundo desde Express - cambios gerchoque! - cambio 3');
+  res.send('¡Hola Mundo desde Express - cambios gerchoque! - cambio 3 - 4');
 });
 
 app.listen(3000, () => {
   console.log('Servidor Express escuchando en el puerto 3000');
   console.log('pruebas de cambios git');
   console.log('pruebas 3');
+  console.log('pruebas 4');
 });
